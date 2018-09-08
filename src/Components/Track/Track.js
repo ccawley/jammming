10d336954a6ed6b2739eeb7d59997b8873d6044a
Curtis Cawley <curtis.cawley@icloud.com>
2018-09-07 22:54:28 -0700

    initial html, css and file structure

---
+++ src/Components/Track/Track.js
@@ -0,0 +1,26 @@
+import React, { Component } from 'react';
+import './Track.css'
+
+class Track extends Component {
+  renderAction() {
+    if (this.isRemoval) {
+      return <a href='#'>-</a>;
+    } else {
+      return <a href='#'>+</a>;
+    }
+  }
+
+  render () {
+    return (
+      <div className="Track">
+        <div className="Track-information">
+          {/* <h3>{<!-- track name will go here -->}</h3> */}
+          {/* <p>{<!-- track artist will go here--> | <!-- track album will go here -->}</p> */}
+        </div>
+        {/* <a className="Track-action"><!-- + or - will go here --></a> */}
+      </div>
+    );
+  }
+}
+
+export default Track;
