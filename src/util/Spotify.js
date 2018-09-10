f1a30ade72fdadd9dceb09337db5b86e80b8497f
Curtis Cawley <curtis.cawley@icloud.com>
2018-09-10 15:11:24 -0700

    commented out some old code

--- src/util/Spotify.js
+++ src/util/Spotify.js
@@ -17,23 +17,23 @@ let Spotify = {
       }
       window.setTimeout(() => usersAccessToken = '', expirationTime * 1000);
       window.history.pushState('Access Token', null, '/');
-      async() => {
-        try {
-          const response = await fetch(`https://accounts.spotify.com/authorize?${clientId}&response_type=token&scope=playlist-modify-public&${redirectUri}`)
-          if (response.ok) {
-            usersAccessToken = window.location.href.match(/access_token=([^&]*)/);
-            expirationTime = window.location.href.match(/expires_in=([^&]*)/);
-            window.setTimeout(() => usersAccessToken = '', expirationTime * 1000);
-            window.history.pushState('Access Token', null, '/');
-          }
-          throw new Error('Request failed!');
-        } catch (error) {
-          console.log(error)
-        };
-      };
-    } else {
-      window.location.href = `https://accounts.spotify.com/authorize?${clientId}&response_type=token&scope=playlist-modify-public&${redirectUri}`;
-    }
+    //   async() => {
+    //     try {
+    //       const response = await fetch(`https://accounts.spotify.com/authorize?${clientId}&response_type=token&scope=playlist-modify-public&${redirectUri}`)
+    //       if (response.ok) {
+    //         usersAccessToken = window.location.href.match(/access_token=([^&]*)/);
+    //         expirationTime = window.location.href.match(/expires_in=([^&]*)/);
+    //         window.setTimeout(() => usersAccessToken = '', expirationTime * 1000);
+    //         window.history.pushState('Access Token', null, '/');
+    //       }
+    //       throw new Error('Request failed!');
+    //     } catch (error) {
+    //       console.log(error)
+    //     };
+    //   };
+    // } else {
+    //   window.location.href = `https://accounts.spotify.com/authorize?${clientId}&response_type=token&scope=playlist-modify-public&${redirectUri}`;
+    // }
     }
   }
 
