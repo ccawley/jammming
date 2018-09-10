34482537685522ac64a795d1797912b6bf14a0b0
Curtis Cawley <curtis.cawley@icloud.com>
2018-09-10 13:46:24 -0700

    refactored my code after reading spotify docs again

--- src/util/Spotify.js
+++ src/util/Spotify.js
@@ -8,23 +8,32 @@ let Spotify = {
     if (usersAccessToken) {
       return usersAccessToken;
     } else if (!usersAccessToken) {
-      async() => {
-        try {
-          const response = await fetch(`https://accounts.spotify.com/authorize?${clientId}&response_type=token&scope=playlist-modify-public&${redirectUri}`)
-          if (response.ok) {
-            usersAccessToken = window.location.href.match(/access_token=([^&]*)/);
-            expirationTime = window.location.href.match(/expires_in=([^&]*)/);
-            window.setTimeout(() => usersAccessToken = '', expirationTime * 1000);
-            window.history.pushState('Access Token', null, '/');
-          }
-          throw new Error('Request failed!');
-        } catch (error) {
-          console.log(error)
-        };
-      };
-    } else {
-      window.location(`https://accounts.spotify.com/authorize?${clientId}&response_type=token&scope=playlist-modify-public&${redirectUri}`)
-    }
+      window.location.href = `https://accounts.spotify.com/authorize?${clientId}&response_type=token&scope=playlist-modify-public&${redirectUri}`;
+      let urlResponse = window.location.href;
+      usersAccessToken = urlResponse.match(/access_token=([^&]*)/);
+      expirationTime = urlResponse.match(/expires_in=([^&]*)/);
+      if (!usersAccessToken || !expirationTime) {
+        window.location.href = `https://accounts.spotify.com/authorize?${clientId}&response_type=token&scope=playlist-modify-public&${redirectUri}`);
+      }
+      window.setTimeout(() => usersAccessToken = '', expirationTime * 1000);
+      window.history.pushState('Access Token', null, '/');
+    //   async() => {
+    //     try {
+    //       const response = await fetch(`https://accounts.spotify.com/authorize?${clientId}&response_type=token&scope=playlist-modify-public&${redirectUri}`)
+    //       if (response.ok) {
+    //         usersAccessToken = window.location.href.match(/access_token=([^&]*)/);
+    //         expirationTime = window.location.href.match(/expires_in=([^&]*)/);
+    //         window.setTimeout(() => usersAccessToken = '', expirationTime * 1000);
+    //         window.history.pushState('Access Token', null, '/');
+    //       }
+    //       throw new Error('Request failed!');
+    //     } catch (error) {
+    //       console.log(error)
+    //     };
+    //   };
+    // } else {
+    //   window.location.href = `https://accounts.spotify.com/authorize?${clientId}&response_type=token&scope=playlist-modify-public&${redirectUri}`;
+    // }
   }
 
 };
