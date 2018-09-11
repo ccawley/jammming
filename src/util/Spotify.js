a891fa24513074b2b4a9da90082c2edf762728ce
Curtis Cawley <curtis.cawley@icloud.com>
2018-09-10 20:52:26 -0700

    deleted a bunch of logs

--- src/util/Spotify.js
+++ src/util/Spotify.js
@@ -3,28 +3,21 @@ let expirationTime;
 const clientId = 'client_id=e1544d3bed444788bb6a705c22207d64';
 const redirectUri = '&redirect_uri=http://localhost:3000/';
 
-let Spotify = {
+const Spotify = {
   getAccessToken() {
     if (usersAccessToken) {
-      console.log(usersAccessToken, '1st');
       return usersAccessToken;
     } else if (!usersAccessToken) {
       window.location.href = `https://accounts.spotify.com/authorize?${clientId}&response_type=token&scope=playlist-modify-public&${redirectUri}`;
-      console.log(usersAccessToken, '2nd');
       let urlResponse = window.location.href;
       usersAccessToken = urlResponse.match(/access_token=([^&]*)/)[0];
       expirationTime = urlResponse.match(/expires_in=([^&]*)/);
       expirationTime = parseInt(expirationTime[0], 0);
-      console.log(usersAccessToken, '3rd');
-      console.log(expirationTime, '1st');
       if (!usersAccessToken || !expirationTime) {
-        console.log(usersAccessToken, '4th');
         window.location.href = `https://accounts.spotify.com/authorize?${clientId}&response_type=token&scope=playlist-modify-public&${redirectUri}`;
-        console.log(usersAccessToken, '5th');
       } else {
         window.setTimeout(() => usersAccessToken = '', expirationTime * 1000);
         window.history.pushState('Access Token', null, '/');
-        console.log(usersAccessToken, '6th');
         return usersAccessToken;
       }
     }
