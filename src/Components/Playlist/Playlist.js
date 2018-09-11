5f88ddb914c5b9216ef66c04fa0ad916b70f958b
Curtis Cawley <curtis.cawley@icloud.com>
2018-09-10 20:14:57 -0700

    deleted some dead/bad code

--- src/Components/Playlist/Playlist.js
+++ src/Components/Playlist/Playlist.js
@@ -9,7 +9,6 @@ class Playlist extends Component {
   }
 
   handleNameChange(e) {
-    // Getting an error about undefined from e.target.value when handleNameChange is inovked in the render method.
     const newPlayListName = e.target.value;
     this.props.onNameChange(newPlayListName);
   }
@@ -17,9 +16,7 @@ class Playlist extends Component {
   render () {
     return (
       <div className="Playlist">
-        {/* Step 59 related??? Swap between the two following lines to get error or not mentioned above... */}
         <input defaultValue={'New Playlist'} onChange={this.handleNameChange}/>
-        {/* <input defaultValue={'New Playlist'} onChange={this.handleNameChange()}/> */}
         <TrackList
           tracks={this.props.playListTracks}
           onRemove={this.props.onRemove}
